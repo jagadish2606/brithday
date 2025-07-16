2a9f69c50babd117b30f558afb8f39ab150721f2
jagadish2606 <jagadishkamil.141@gmail.com>
2025-07-16 21:44:55 +0530

    up

--- src/components/BirthdayWish.jsx
+++ src/components/BirthdayWish.jsx
@@ -292,6 +292,7 @@ const BirthdayWish = () => {
                 whileTap={{ scale: 0.9 }} 
                 className="bg-gradient-to-r from-purple-500 to-pink-500 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
               >
+                <p className='me-2'>People’s Diary</p>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v1h8v-1zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-1a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v1h-3zM4.75 12.094A5.973 5.973 0 004 15v1H1v-1a3 3 0 013.75-2.906z" />
                 </svg>
